6c12dd1cefa64b2f0cde4b98804307eb3d11967a
Shrike0p <prakharsingh2208@gmail.com>
2024-03-14 20:23:51 +0530

    01_js

---
+++ JS/01_variables_const.js
@@ -0,0 +1,19 @@
+const accountId=1445677
+let accountEmail="iamshrike0p@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// ┌─────────┬─────────────────────────┐
+// │ (index) │ Values                  │
+// ├─────────┼─────────────────────────┤
+// │ 0       │ 1445677                 │
+// │ 1       │ 'iamshrike0p@gmail.com' │
+// │ 2       │ '12345'                 │
+// │ 3       │ 'Jaipur'                │
+// │ 4       │ undefined               │
+// └─────────┴─────────────────────────┘
+
+//Do not use var because of scope issue
